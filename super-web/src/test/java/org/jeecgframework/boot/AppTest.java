b60f1214859571aab2a48df4220cf3d0cd176eec
无极 <wgzhxy@163.com>
2021-12-21 13:44:07 +0800

    update

---
+++ super-web/src/test/java/org/jeecgframework/boot/AppTest.java
@@ -0,0 +1,14 @@
+package org.jeecgframework.boot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/** Unit test for simple App. */
+public class AppTest {
+  /** Rigorous Test :-) */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
